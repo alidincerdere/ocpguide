3881d3c9a361a16f4da2ffc71069471669026321
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2019-03-21 10:08:27 +0300

    removing unused packet

--- src/main/java/com/ocppreperation/ocpguide/WebService/CompileRequestController.java
+++ src/main/java/com/ocppreperation/ocpguide/WebService/CompileRequestController.java
@@ -3,7 +3,6 @@ package com.ocppreperation.ocpguide.WebService;
 import com.ocppreperation.ocpguide.Model.CompileInput;
 import com.ocppreperation.ocpguide.Model.CompileOutput;
 import com.ocppreperation.ocpguide.Service.CompilerRestClient;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
